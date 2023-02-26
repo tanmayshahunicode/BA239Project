7cc6b25b85c7dc513ecf1878e575d00cbac9c064
Admin <Admin@DESKTOP-GS0CP1T>
2023-02-26 11:22:38 +0530

    TestCase 1 is modified and TestCase2 is added.

--- src/gitPackage/TestCase1.java
+++ src/gitPackage/TestCase1.java
@@ -5,6 +5,7 @@ public class TestCase1 {
 	public static void main(String[] args) {
 
 		System.out.println("This is TestCase 1");
+		System.out.println("Modifying Test Case 1---Extra Code");
 	}
 
 }
